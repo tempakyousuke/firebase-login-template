a219c2983a55fc3d3f57a56c4e7003a3f4b2cfe8
tempakyousuke <tempakyousuke@gmail.com>
2019-07-13 15:38:37 +0000

    fix style

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,9 +37,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-    "@/plugins/firebase"
-  ],
+  plugins: ['@/plugins/firebase'],
   /*
    ** Nuxt.js modules
    */
